70bdab8f9a1b0118f0d6012d92ec0bd50fc1f388
ftpaul <ftpaul@gmail.com>
2013-09-08 15:50:26 +0100

    bootstrap e login

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Sgs::Application.config.secret_key_base = '289d58db9065b57b735a4023c8b6079e04b76459208f589d02152dcdeb9d01ba5567fbc9f271dcba5dd28ebfa0e53ac179aa97592e55000f407d99ee25fd5ffe'
+Sgs::Application.config.secret_token = '070d0863071941e0bbe8c30b03b498ccecda323154916b2f1b57a13a5b9b74cdc73da97e99a018beda7e765dcc1e7d79fe92c284e0e0167db3005070a675850a'
